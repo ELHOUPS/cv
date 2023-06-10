266f7b8a8de08b9e4b1fdbe1fd569370133d29b5
kafi lotfi <122447827+ELHOUPS@users.noreply.github.com>
2023-06-10 13:59:20 +0000

    cv app

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route('/more')
+def more():
+    return render_template("more.html", title="more about !! ")
+@app.route('/education')
+def education():
+    return render_template("education.html", title=" éducation Resume")
+
+@app.route('/experience')
+def experience():
+    return render_template("experience.html", title=" éxpereience Resume")
